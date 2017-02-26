997dc0ff659428b136da8f0717a0bd2225ee75ab
peakview <kepler@126.com>
2017-02-26 23:30:09 +0800

    Tx with index in the frond of string

--- CC1101TXtest.lua
+++ CC1101TXtest.lua
@@ -3,7 +3,7 @@ CCTxTimer = tmr.create()
 iiTx=1
 -- oo calling
 CCTxTimer:register(2000, tmr.ALARM_AUTO, 
-function (t) CCxmit("Its brightest stars are Rigel Beta Orionis + Betel-" .. iiTx .."\n"); iiTx=iiTx+1;  end)
+function (t) CCxmit("#" .. iiTx .."Its brightest stars are Rigel Beta Orionis + Bete" .. "\n"); iiTx=iiTx+1;  end)
 CCTxTimer:start()
 
 --CCTxTimer:unregister()
